b16ba03a742c596566fa222a17127e597bd0221d
ilotterytea <iltsu@alright.party>
2025-05-27 15:01:38 +0500

    feat: mirrors

--- lib/partials.php
+++ lib/partials.php
@@ -29,6 +29,19 @@ function html_footer()
 
     echo '' ?>
     <footer class="column justify-center align-center gap-8">
+        <?php if (array_key_exists(INSTANCE_URL, INSTANCE_MIRRORS)): ?>
+            <p>You are using a mirror for <?= INSTANCE_MIRRORS[INSTANCE_URL] ?>. <a href="<?= INSTANCE_ORIGINAL_URL ?>">[ Check
+                    out the origin website ]</a></p>
+        <?php elseif (!empty(INSTANCE_MIRRORS)): ?>
+            <div class="row gap-8">
+                <p>Mirrors:</p>
+                <ul class="row gap-4" style="list-style: none;">
+                    <?php foreach (INSTANCE_MIRRORS as $url => $name): ?>
+                        <li><a href="<?= $url ?>"><?= $name ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endif; ?>
         <p>Serving <?= count($files) ?> files and <?= sprintf("%.2f", $file_size) ?>MB of active content</p>
     </footer>
     <?php ;
