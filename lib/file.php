19e8aa1cd880f169cbf379746f19098b90514a97
ilotterytea <iltsu@alright.party>
2026-02-16 00:57:58 +0500

    feat: snowflake id generation

--- lib/file.php
+++ lib/file.php
@@ -1,6 +1,48 @@
 <?php
 include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/config.php";
 
+function generate_snowflake_id(int $machineId = 1, int $epoch = 1609459200000): int
+{
+    static $last_timestamp = 0;
+    static $sequence = 0;
+
+    $machine_id_bits = 10;
+    $sequence_bits = 12;
+
+    $max_machine_id = (1 << $machine_id_bits) - 1;
+    $max_sequence = (1 << $sequence_bits) - 1;
+
+    if ($machineId < 0 || $machineId > $max_machine_id) {
+        throw new InvalidArgumentException("Machine ID must be between 0 and $max_machine_id");
+    }
+
+    $timestamp = (int) floor(microtime(true) * 1000);
+
+    if ($timestamp < $last_timestamp) {
+        throw new RuntimeException("Clock moved backwards. Refusing to generate ID.");
+    }
+
+    if ($timestamp === $last_timestamp) {
+        $sequence = ($sequence + 1) & $max_sequence;
+
+        if ($sequence === 0) {
+            do {
+                $timestamp = (int) floor(microtime(true) * 1000);
+            } while ($timestamp <= $last_timestamp);
+        }
+    } else {
+        $sequence = 0;
+    }
+
+    $last_timestamp = $timestamp;
+
+    return
+        (($timestamp - $epoch) << ($machine_id_bits + $sequence_bits)) |
+        ($machineId << $sequence_bits) |
+        $sequence;
+}
+
+
 function parse_file_name(string|null $filename): array|null
 {
     if ($filename === null) {
