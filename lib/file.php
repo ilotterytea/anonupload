577c7e80f4baef52f91f993be696afe0636c2630
ilotterytea <tea@shungites.casa>
2026-06-18 18:00:30 +0300

    upd: calculate FileMetadata using getID3, getimagesize and more

--- lib/file.php
+++ lib/file.php
@@ -1,55 +1,6 @@
 <?php
+include_once "{$_SERVER['DOCUMENT_ROOT']}/vendor/autoload.php";
 include_once "{$_SERVER['DOCUMENT_ROOT']}/lib/config.php";
-
-function get_file_metadata(string $file_path): array|null
-{
-    $ext = explode('.', basename($file_path));
-    $ext = $ext[count($ext) - 1];
-    $mime = CONFIG['upload']['acceptedmimetypes'][$ext];
-
-    if (str_starts_with($mime, 'image/') && IMAGEMAGICK_COMMAND) {
-        [$width, $height] = explode('x', trim(shell_exec(IMAGEMAGICK_COMMAND['identify'] . ' -format "%wx%h" ' . escapeshellarg($file_path) . '[0]')));
-        return [
-            'width' => intval($width),
-            'height' => intval($height),
-            'duration' => null,
-            'line_count' => null
-        ];
-    } else if (str_starts_with($mime, 'video/')) {
-        $info = shell_exec('ffprobe -v error -select_streams v:0 -show_entries stream=width,height,duration -of csv=p=0 ' . escapeshellarg($file_path));
-        [$width, $height, $duration] = explode(',', trim($info));
-        return [
-            'width' => intval($width),
-            'height' => intval($height),
-            'duration' => $duration == 'N/A' ? null : intval(round($duration, 2)),
-            'line_count' => null
-        ];
-    } else if (str_starts_with($mime, 'audio/')) {
-        return [
-            'width' => null,
-            'height' => null,
-            'duration' => intval(round(trim(shell_exec('ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ' . escapeshellarg($file_path))), 2)),
-            'line_count' => null
-        ];
-    } else if (str_starts_with($mime, 'text/')) {
-        return [
-            'width' => null,
-            'height' => null,
-            'duration' => null,
-            'line_count' => intval(trim(shell_exec('wc -l < ' . escapeshellarg($file_path))))
-        ];
-    } else if ($mime === 'application/x-shockwave-flash') {
-        [$width, $height] = parse_swf_file($file_path);
-        return [
-            'width' => $width,
-            'height' => $height,
-            'duration' => null,
-            'line_count' => null
-        ];
-    } else {
-        return null;
-    }
-}
 
 function is_swf_file($filename)
 {
@@ -161,146 +112,6 @@ function strip_exif(string $file_path)
     return empty($output);
 }
 
-function remove_video_letterbox(string $input_path, string $output_path)
-{
-    $input_path = escapeshellarg($input_path);
-    $output_path = escapeshellarg($output_path);
-    $output = shell_exec("ffmpeg -nostdin -i $input_path -vf cropdetect=24:16:0 -f null - 2>&1");
-
-    if (preg_match_all('/crop=\d+:\d+:\d+:\d+/', $output, $matches)) {
-        $area = end($matches);
-        $area = end($area);
-    } else {
-        throw new RuntimeException('Could not detect crop parameters. Try upload the video without "Remove letterbox" option.');
-    }
-
-    $area = escapeshellarg($area);
-
-    $output = [];
-    exec("ffmpeg -nostdin -i $input_path -vf $area -c:a copy $output_path 2>&1", $output, $code);
-
-    return $code == 0;
-}
-
-function parse_zip_web_archive(string $input_path, string $output_path)
-{
-    $allowed_extensions = [
-        "html",
-        "js",
-        "css",
-        "png",
-        "jpg",
-        "jpeg",
-        "gif",
-        "mp3",
-        "ogg",
-        "wasm",
-        "atlas",
-        "skin",
-        "txt",
-        "fnt",
-        "json",
-        "glb",
-        "glsl",
-        "map",
-        "teavmdbg",
-        "xml",
-        "ds_store",
-    ];
-    $max_total_uncompressed = 128 * 1024 * 1024;
-    $max_file_size = 32 * 1024 * 1024;
-
-    $zip = new ZipArchive();
-    if ($zip->open($input_path) !== true) {
-        throw new RuntimeException("Invalid ZIP");
-    }
-
-    $is_webapp = false;
-    for ($i = 0; $i < $zip->numFiles; $i++) {
-        $stat = $zip->statIndex($i);
-        $is_webapp = $stat["name"] == "index.html";
-        if ($is_webapp) {
-            break;
-        }
-    }
-
-    if (!$is_webapp) {
-        $zip->close();
-        return $is_webapp;
-    }
-
-    $total_uncompressed = 0;
-    for ($i = 0; $i < $zip->numFiles; $i++) {
-        $stat = $zip->statIndex($i);
-        $path = $stat["name"];
-        if (strpos($path, "..") !== false) {
-            throw new RuntimeException("Invalid file path");
-        }
-
-        if (
-            strpos($path, "__MACOSX/") === 0 ||
-            (basename($path)[0] === "." &&
-                strstr(basename($path), 0, 2) === "._")
-        ) {
-            continue;
-        }
-
-        $ext = strtolower(pathinfo($path, PATHINFO_EXTENSION));
-        error_log($ext);
-        if (!in_array($ext, $allowed_extensions) && $stat["size"] > 0) {
-            throw new RuntimeException(
-                "Forbidden file type in the archive: $path",
-            );
-        }
-
-        $total_uncompressed += $stat["size"];
-        if (
-            $total_uncompressed > $max_total_uncompressed ||
-            $stat["size"] > $max_file_size
-        ) {
-            throw new RuntimeException("ZIP too large when uncompressed");
-        }
-    }
-
-    mkdir($output_path, 0755, true);
-    if (!$zip->extractTo($output_path)) {
-        rmdir($output_path);
-        throw new RuntimeException("ZIP extraction failed");
-    }
-
-    $zip->close();
-
-    return $is_webapp;
-}
-
-function convert_file(string $input_path, string $output_path)
-{
-    $input_ext = pathinfo($input_path, PATHINFO_EXTENSION);
-    $output_ext = pathinfo($output_path, PATHINFO_EXTENSION);
-
-    $input_mime = CONFIG['upload']['acceptedmimetypes'][$input_ext] ?? null;
-    $output_mime = CONFIG['upload']['acceptedmimetypes'][$output_ext] ?? null;
-
-    if (str_starts_with($input_mime, 'video/') && str_starts_with($output_mime, 'video/')) {
-        $copy_supported = match (strtolower($output_ext)) {
-            'mp4', 'mov', 'mkv', 'webm', 'avi' => true,
-            default => false,
-        };
-
-        $cmd = $copy_supported
-            ? sprintf('ffmpeg -i %s -c copy -y %s 2>&1', escapeshellarg($input_path), escapeshellarg($output_path))
-            : sprintf('ffmpeg -i %s -c:v libx264 -c:a aac -y %s 2>&1', escapeshellarg($input_path), escapeshellarg($output_path));
-
-        exec($cmd, $output, $return_var);
-
-        if ($return_var !== 0) {
-            throw new RuntimeException("Conversion failed: " . implode("\n", $output));
-        }
-    } else {
-        throw new RuntimeException("MIME types must be matched: $input_mime -> $output_mime");
-    }
-}
-
 class FileOptions
 {
     public string|null $content_type;
@@ -313,27 +124,35 @@ class FileMetadata
     public static function from_file_path(string $input_path, string $mime): FileMetadata|null
     {
         $m = null;
-
-        if (str_starts_with($mime, 'image/') && IMAGEMAGICK_COMMAND) {
-            [$w, $h] = explode('x', trim(shell_exec(IMAGEMAGICK_COMMAND['identify'] . ' -format "%wx%h" ' . escapeshellarg($input_path) . '[0]')));
-
-            $m = new FileMetadata();
-            $m->width = intval($w);
-            $m->height = intval($h);
-        } else if (str_starts_with($mime, 'video/')) {
-            $info = shell_exec('ffprobe -v error -select_streams v:0 -show_entries stream=width,height,duration -of csv=p=0 ' . escapeshellarg($input_path));
-            [$w, $h, $d] = explode(',', trim($info));
+        if (str_starts_with($mime, 'image/')) {
+            [$w, $h] = getimagesize($input_path);
 
             $m = new FileMetadata();
             $m->width = intval($w);
             $m->height = intval($h);
-            $m->duration = $d == 'N/A' ? null : intval(round($d, 2));
-        } else if (str_starts_with($mime, 'audio/')) {
+        } else if (str_starts_with($mime, 'video/') || str_starts_with($mime, 'audio/')) {
+            $getID3 = new getID3();
+            $info = $getID3->analyze($input_path);
             $m = new FileMetadata();
-            $m->duration = intval(round(trim(shell_exec('ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ' . escapeshellarg($input_path))), 2));
+
+            if (isset($info['video']['resolution_x'])) {
+                $m->width = (int) $info['video']['resolution_x'];
+            }
+
+            if (isset($info['video']['resolution_y'])) {
+                $m->height = (int) $info['video']['resolution_y'];
+            }
+
+            if (isset($info['playtime_seconds'])) {
+                $m->duration = (int) round($info['playtime_seconds']);
+            }
         } else if (str_starts_with($mime, 'text/')) {
             $m = new FileMetadata();
-            $m->line_count = intval(trim(shell_exec('wc -l < ' . escapeshellarg($input_path))));
+
+            $file = new SplFileObject($input_path);
+            $file->seek(PHP_INT_MAX);
+
+            $m->line_count = $file->key() + 1;
         }
 
         return $m;
